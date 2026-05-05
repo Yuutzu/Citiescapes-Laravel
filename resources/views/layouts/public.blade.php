32a0b25c042c120985428ef65c8a9c6ca4469fa3
Yuutzu <j.polinar.540369@umindanao.edu.ph>
2026-05-05 19:40:10 +0800

    chore: ignore compiled view cache

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -8,6 +8,8 @@
     <title>{{ $title ?? 'Citiescapes — Rooms' }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
